82f270fe6c753886557d7b2754d2211ba21c650f
Wesley <wesaboo@gmail.com>
2020-06-21 16:57:46 -0700

    adds readme, finish app.js, change location of html file to output folder

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# TeamProfile-Generator
+
+![License Badge](https://img.shields.io/badge/License-ISC-green.svg)
+
+### Github Username: wesaboo
+
+Github Email Address: wesaboo@gmail.com
+
+## Project/Repo Title: Team Profile Generator
+
+Description: Powered by NodeJS, Can be use to take information entered via the inquirer prompt in order to generate an HTML profile page for members of a Development Team
+
+Table of Contents: 
+
+Installation Instructions: Must run npm install before running, requires NodeJS. To start, navigate to the directory of the generator and type in "node app" within the terminal.
+
+How to use: When app is started you will be prompted with questions. The answer provided will be used to generate the HTML.
+
+![Screenshot](/asset/screenshot/Terminal.png)
+
+![Screenshot](/asset/screenshot/htmlScreenshot.png)
+
+License: ISC
+
+Contributor: Wesley Tran
+
+Test Code: 
+
+![Avatar](https://avatars0.githubusercontent.com/u/60164964?v=4)
--- app.js
+++ app.js
@@ -4,147 +4,150 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
-const outputPath = path.join(__dirname, "team.html");
 const render = require("./lib/htmlRenderer");
-const { create } = require("domain");
-const team = []
 
-const managerQuestions = () => {
-    inquirer
-        .prompt([
-            {
-                type: "input",
-                message: "What is your name (manager)?",
-                name: "name"
-            },
-            {
-                type: "input",
-                message: "Enter your id number?",
-                name: "id"
-            },
-            {
-                type: "input",
-                message: "Enter your email?",
-                name: "email"
-            },
-            {
-                type: "input",
-                message: "Enter your office number?",
-                name: "officenumber"
-            }
-        ]).then(response => {
-            const manager = new Manager(
-                response.name,
-                response.id,
-                response.email,
-                response.officenumber
-            )
-            team.push(manager)
-            addteamMember()
-        }).catch(err => {
-            throw err
-        })
-}
-const addteamMember = () => {
-    inquirer
-        .prompt([
-            {
-                type: "list",
-                message: "Which role would you like to add to your team?",
-                name: "chooseteam",
-                choices: [
-                    "Engineer",
-                    "Intern",
-                    "I do not wish to add any more team members"
-                ]
-            }
-        ]).then(response => {
-            switch(response.chooseteam) {
-                case "Engineer":
-                    createEngineer()
-                    break
-                case "Intern":
-                    createIntern()
-                    break
-                default:
-                    fs.writeFileSync(outputPath, render(team), "utf8")
-                    break
-            }
-        }).catch(err => {
-            throw err
-        })
-}
-const createEngineer = () => {
-    inquirer
-        .prompt([
-            {
-                type: "input",
-                message: "What is the engineer's name?",
-                name: "name"
-            },
-            {
-                type: "input",
-                message: "Enter their id number?",
-                name: "id"
-            },
-            {
-                type: "input",
-                message: "Enter their email?",
-                name: "email"
-            },
-            {
-                type: "input",
-                message: "Enter their GitHub userid?",
-                name: "github"
-            }
-        ]).then(response => {
-            const engineer = new Engineer(
-                response.name,
-                response.id,
-                response.email,
-                response.github
-            )
-            team.push(engineer)
-            addteamMember()
-        }).catch(err => {
-            throw err
-        })
-}
-const createIntern = () => {
-    inquirer
-        .prompt([
-            {
-                type: "input",
-                message: "What is the intern's name?",
-                name: "name"
-            },
-            {
-                type: "input",
-                message: "Enter their id number?",
-                name: "id"
-            },
-            {
-                type: "input",
-                message: "Enter their email?",
-                name: "email"
-            },
-            {
-                type: "input",
-                message: "What school do they attend?",
-                name: "school"
-            }
-        ]).then(response => {
-            const intern = new Intern(
-                response.name,
-                response.id,
-                response.email,
-                response.school
-            )
-            team.push(intern)
-            addteamMember()
-        }).catch(err => {
-            throw err
-        })
-}
-managerQuestions()
+// Output team.html into output folder within directory - Fails if output folder is not created first
+const output = path.join(__dirname + "/output/" , "team.html");
+// Empty array to contain response from inquirer
+const team = [];
+
+// Gets response to create manager profile THEN pushes to empty array "team", THEN calls newTeamMember function
+const newManager = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Please enter the Manager's name",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Please enter the ID number for the employee",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "Please enter the email for the employee",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "Please enter the phone number for the employee",
+            name: "phoneNumber"
+        }
+    ]).then(response => {
+        const manager = new Manager (
+            response.name,
+            response.id,
+            response.email,
+            response.phoneNumber
+        );
+        team.push(manager);
+        newTeamMember();
+    }).catch(err => {
+        throw err
+    })
+};
+
+// Function to select role of next team member to add, THEN calls the corresponding function
+// If no more employee needs to be added, file is written to team.html in output folder
+const newTeamMember = () => {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What is the role for the employee",
+            name:"role",
+            choices: [
+                "Engineer",
+                "Intern",
+                "No more employees to add"
+            ]
+        }
+    ]).then(function (response) {
+        if (response.role === "Engineer") {
+            addEngineer();
+        } else if (response.role === "Intern") {
+            addIntern();
+        } else {
+            fs.writeFile(output, render(team), (err) => {
+                if (err) throw err;
+                console.log('The file has been saved!');
+            });
+        }
+    })
+};
+
+// Gets response to create engineer profile THEN pushes to empty array "team", THEN calls newTeamMember function
+const addEngineer = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Please enter the Engineer's name",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Please enter the ID number for the employee",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "Please enter the email for the employee",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "Please enter the Github Username for the employee",
+            name: "github"
+        }
+    ]).then(response => {
+        const engineer = new Engineer (
+            response.name,
+            response.id,
+            response.email,
+            response.github
+        );
+        team.push(engineer);
+        newTeamMember();
+    }).catch(err => {
+        throw err
+    })
+};
+
+// Gets response to create intern profile THEN pushes to empty array "team", THEN calls newTeamMember function
+const addIntern = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Please enter the Intern's name",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Please enter the ID number for the employee",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "Please enter the email for the employee",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "Please enter the school name for the employee",
+            name: "school"
+        }
+    ]).then(response => {
+        const intern = new Intern (
+            response.name,
+            response.id,
+            response.email,
+            response.school
+        );
+        team.push(intern);
+        newTeamMember();
+    }).catch(err => {
+        throw err
+    })
+};
+
+newManager();
